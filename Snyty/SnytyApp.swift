bb75011960994b79a483b271819f0d5d83380fcc
Danylo Maister <mayster.danya@gmail.com>
2026-04-25 20:32:02 +0300

    Initial Commit

---
+++ Snyty/SnytyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnytyApp.swift
+//  Snyty
+//
+//  Created by vvelon on 25.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct SnytyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
